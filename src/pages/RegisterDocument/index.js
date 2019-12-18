aff9251289f8f369506187514842101b2c5a7733
pamellasds <pamella.soares@aluno.uece.br>
2019-12-18 19:49:47 -0300

    Alterações

--- src/pages/RegisterDocument/index.js
+++ src/pages/RegisterDocument/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import ReactDOM from 'react-dom';
 
-export default function RegisterDocument({ history }){
+export default function RegisterDocument({ history }) {
 
     const [name, setName] = useState('');
     const [matricula, setMatricula] = useState('');
@@ -9,7 +10,61 @@ export default function RegisterDocument({ history }){
     const [sexo, setSexo] = useState('');
     const [filiacao, setFiliacao] = useState('');
 
-    async function handleSubmit(e){
+    class App extends React.Component {
+        state = { show: false }
+
+        showModal = () => {
+            this.setState({ show: true });
+        }
+
+        hideModal = () => {
+            this.setState({ show: false });
+        }
+
+        render() {
+            return (
+                <main>
+                    <Modal show={this.state.show} handleClose={this.hideModal} >
+                        <div className="input-modal">
+                            <h3>Assinatura Digital por meio da "Certisign"</h3>
+                            <p style={{ padding: "15px 15px 15px 15px" }}>Introduzir PIN</p>
+                            <input
+                                id="pin"
+                                type="text"
+                                placeholder="PIN"
+                            >
+                            </input>
+                        </div>
+                    </Modal>
+                    <div>
+                        <button className="btn" onClick={this.showModal} type="button">Assinar</button>
+                    </div>
+                </main>
+            )
+        }
+    }
+
+    const Modal = ({ handleClose, show, children }) => {
+        const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+
+        return (
+            <div className={showHideClassName}>
+                <section className="modal-main">
+                    {children}
+                    <div>
+                        <button className="btn" type="submit">Confirmar</button>
+                    </div>
+                </section>
+            </div>
+        );
+    };
+
+
+    const container = document.createElement('div');
+    //document.body.appendChild(container);
+    ReactDOM.render(<App />, container);
+
+    async function handleSubmit(e) {
         e.preventDefault();
         localStorage.setItem('name', name);
         localStorage.setItem('matricula', matricula);
@@ -35,7 +90,7 @@ export default function RegisterDocument({ history }){
                     placeholder="Digite o nome a ser registrado"
                     value={name}
                     onChange={event => setName(event.target.value)}
-                    >
+                >
                 </input>
 
                 <label htmlFor="matricula">Matrícula</label>
@@ -45,8 +100,8 @@ export default function RegisterDocument({ history }){
                     placeholder="Digite a matrícula"
                     value={matricula}
                     onChange={event => setMatricula(event.target.value)}
-                    >
-                </input>     
+                >
+                </input>
 
                 <label htmlFor="municipioNasc">Município</label>
                 <input
@@ -55,8 +110,8 @@ export default function RegisterDocument({ history }){
                     placeholder="Digite o município"
                     value={municipioNasc}
                     onChange={event => setMunicipioNasc(event.target.value)}
-                    >
-                </input> 
+                >
+                </input>
 
                 <label htmlFor="ufNasc">UF</label>
                 <input
@@ -65,8 +120,8 @@ export default function RegisterDocument({ history }){
                     placeholder="Digite o UF"
                     value={ufNasc}
                     onChange={event => setUfNasc(event.target.value)}
-                    >
-                </input>    
+                >
+                </input>
 
                 <label htmlFor="sexo">Sexo</label>
                 <input
@@ -75,8 +130,8 @@ export default function RegisterDocument({ history }){
                     placeholder="Digite o sexo"
                     value={sexo}
                     onChange={event => setSexo(event.target.value)}
-                    >
-                </input>       
+                >
+                </input>
 
                 <label htmlFor="filiacao">Filiação</label>
                 <input
@@ -85,12 +140,10 @@ export default function RegisterDocument({ history }){
                     placeholder="Digite o nome do pai e da mãe"
                     value={filiacao}
                     onChange={event => setFiliacao(event.target.value)}
-                    >
-                </input>  
+                >
+                </input>
 
-                <div>
-                    <button className="btn" type="submit">Assinar</button>
-                </div>
+                <App />
             </form>
         </>
     );
