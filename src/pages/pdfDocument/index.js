4efaf1b531b6b15389f7e7c34eb70de3aee15ef4
pamellasds <pamella.soares@aluno.uece.br>
2019-12-18 22:37:14 -0300

    ajustes

--- src/pages/pdfDocument/index.js
+++ src/pages/pdfDocument/index.js
@@ -48,7 +48,7 @@ export default function pdfDocument({ history }) {
     }
 
     const response = await api.post('/certificate', certidao);
-    await sleep(10000);
+    await sleep(1000);
     console.log('asdasd', response);
     console.log(response.data.success);
     
